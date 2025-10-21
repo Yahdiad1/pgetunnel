052a40f81e00d294918a014f398155a87aedd283
Yahdiad1 <yahdiad@gmail.com>
2025-10-21 21:25:47 +0700

    Update developer information in status.sh

--- status.sh
+++ status.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 # System Request : Debian 9+/Ubuntu 18.04+/20+
-# Develovers » Riswanvpn
-# Email      » Riswanvpn@gmail.com
-# telegram   » https://t.me/Riswanvpn
+# Develovers » Yhdsvpn
+# Email      » Yahdiad@gmail.com
 
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-# Riswanvpn
+# Yhdsvpn
 
 # Color
 NC='\e[0m'
